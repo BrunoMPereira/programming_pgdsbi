04f236777db860e87167b18a298395077a7a2a4c
BrunoMiguelPereira <pereirinha_bp@hotmail.com>
2025-02-14 01:02:20 +0000

    Class formulation plus graph function

--- main.py
+++ main.py
@@ -2,42 +2,62 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def input_layer(weights,bias,values):
-    '''
-    First layer that can calculate the computation between weights, values and bias
-    '''
-    result = sum(weights*values) + bias
-    return result
+class Perceptron():
 
-def activation_function(comp_values, probability=0.5):
-    '''
-    Activation function that defines if the output from the input is 0 or 1.
-    Classification layer.
-    '''
-    return np.where(1 / (1 + np.exp(-comp_values)) >= probability,1,0)
+    def __init__(self, iterations, learning_rate, error_iterations, probability, favor_class):
+        self.iterations = iterations
+        self.learning_rate = learning_rate
+        self.error_iterations = error_iterations
+        self.weights = None
+        self.bias = None
+        self.probability = probability
+        self.favor_class = -1 if favor_class == 0 else 1
+        self.errors = []
 
-def loss_function(y,y_pred):
-    '''
-    Calculate the error between real and prediciton value.
-    In this case it will always output 0 or 1
-    '''
-    return (y - y_pred)**2
+    def input_layer(self, input):
+        '''
+        First layer that can calculate the computation between weights, values and bias
+        '''
+        return sum(self.weights*input) + self.bias
+    
+    def activation_function(self, computed_value):
+        '''
+        Activation function that defines if the output from the input is -1 or 1.
+        Classification layer.
+        '''
+        deterministic = 1 if computed_value >= 0 else -1
+        if deterministic == self.favor_class:
+            prediction = self.favor_class if np.random.rand() < self.probability else -self.favor_class
+        else:
+            prediction = deterministic
+        return prediction
+    
+    def predict(self, input):
+        return self.activation_function(self.input_layer(input))
+    
+    def loss_function(self, y, y_pred):
+        '''
+        Calculate the error between real and prediciton value.
+        In this case it will always output 0 or 1
+        '''
+        return int(y != y_pred)
+    
+    def train(self, X, y):
+        y_treated = np.where(y>=1,1,-1)
+        self.weights = np.zeros(X.shape[1])
+        self.bias = 0
 
-def weight_update(weights,bias,y,y_pred,values,learning_rate):
-    '''
-    Updates weights based on the learning rate given, the error and the values
-    '''
-    return weights + learning_rate*(y-y_pred)*values, bias + learning_rate*(y-y_pred)
-
-def compare_errors(errors, i, error_rate):
-    '''
-    Function that allows to evaluate if we achieved the user-defined error.
-    If the error_rate of the user is 1, it means that the user allows the difference between iterations to be 1.
-    '''
-    prev_error = errors[i-1]
-    actual_error = errors[i]
-    return (actual_error - prev_error) <= error_rate
-
+        for _ in range(self.iterations):
+            total_error = 0
+            for row_X, i_y in zip(X,y_treated):
+                y_pred = self.predict(row_X)
+                self.weights += self.learning_rate*y_pred*row_X
+                self.bias += self.learning_rate*y_pred
+                total_error += self.loss_function(i_y,y_pred)
+            self.errors.append(total_error)
+            if total_error <= self.error_iterations:
+                print('Early Stopped.')
+                break
 
 def get_inputs():
     '''
@@ -46,14 +66,15 @@ def get_inputs():
     iterations = int(input('Número iterações: '))
     error_change = int(input('Número de erros na classificação mínimo para parar: '))
     probability_check = float(input('Probabilidade de escolher 1: '))
+    favor_class = int(input('Which class to favor based on probability (0 or 1): '))
     learning_rate = float(input('Learning Rate: '))
-    return iterations, error_change, probability_check, learning_rate
+    amostras = int(input('Número de linhas: '))
+    return iterations, error_change, probability_check, favor_class, learning_rate, amostras
 
-def generate_dataset():
+def generate_classification_dataset(n_rows):
     '''
     Funtion to generate the dataset for study.
     '''
-    n_rows = int(input('Número de linhas: '))
     X_class1 = np.random.randn(n_rows // 2, 2) + np.array([-3, -3])
     X_class2 = np.random.randn(n_rows // 2, 2) + np.array([3, 3])
     # Vertical stack - one on top of the other
@@ -63,28 +84,8 @@ def generate_dataset():
     return X, y
 
 
-if __name__ == "__main__":
-    np.random.seed(42)
-    iterations, error_change, probability_check, learning_rate = get_inputs()
-    X, y = generate_dataset()
-    weights = np.zeros(X.shape[1])
-    bias = np.random.uniform(0,1)
-    error = []
-    for i in range(iterations):
-        total_error = 0
-        for input, target in zip(X,y):
-            comp_values = input_layer(weights,bias,input)
-            y_pred = activation_function(comp_values,probability_check)
-            weights,bias = weight_update(weights,bias,target,y_pred,input,learning_rate)
-            total_error += loss_function(target,y_pred)
-        error.append(total_error)
-        if i >= 1 and compare_errors(error, i, error_change):
-            break
-    # Define class colors (this is optional but can help to manually specify colors)
-    class_colors = ['red', 'green']  # Red, Green
-
-    class_colors_final = ['blue', 'orange']
-
+def plot_data(X, y, colors):
+    ''''''
     # Plot each class with a different color
     for class_id in np.unique(y).tolist():  # Iterate over unique class labels
         # Select points of the current class
@@ -95,28 +96,27 @@ if __name__ == "__main__":
                     label=f'Class {class_id}',  # Label for the legend
                     alpha=0.6, 
                     edgecolors='k', 
-                    color=class_colors[int(class_id)])
+                    color=colors[int(class_id)])
     
     # Add the legend
     plt.legend()
 
     # Display the plot
     plt.show()
+    return
 
+if __name__ == "__main__":
+    np.random.seed(42)
+    iterations, error_change, probability_check, favor_class, learning_rate, linhas = get_inputs()
+    X, y = generate_classification_dataset(linhas)
+    pp = Perceptron(iterations, learning_rate, error_change,probability_check,favor_class)
+    pp.train(X,y)
+    
+    class_colors = ['red', 'green'] 
+    class_colors_train = ['blue', 'orange']
 
-    new_classes = np.array([activation_function(input_layer(weights,bias,input),probability_check) for input,target in zip(X,y)])
-
-    for class_id in [0,1]:  # Iterate over unique class labels
-        # Select points of the current class
-        class_points = X[new_classes == class_id]
-        
-        # Scatter plot for the current class
-        plt.scatter(class_points[:, 0], class_points[:, 1], 
-                    label=f'New Class {class_id}',  # Label for the legend
-                    alpha=0.6, 
-                    edgecolors='k', 
-                    color=class_colors_final[int(class_id)])
-    plt.legend()
-
-    # Display the plot
-    plt.show()
+    plot_data(X, y, class_colors)
+    
+    new_classes = np.array([1 if pp.predict(input) == 1 else 0 for input, _ in zip(X, y)])
+    plot_data(X, new_classes, class_colors_train)
+    
